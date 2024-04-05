9e58730fa17cc22997f53616b5c2321141ca2328
carpowiski1558 <160245828+carpowiski1558@users.noreply.github.com>
2024-04-05 10:56:44 -0300

    atualizaçao 05/04
    atualizaçao 05/04

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-09-05T10:45:00");
+const tempoObjetivo2 = new Date("2024-09-05T20:51:00");
+const tempoObjetivo3 = new Date("2024-09-30T13:22:00");
+const tempoObjetivo4 = new Date("2024-09-01T15:12:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
